2ef5888186038ea6aeadaf6720be742316ec29aa
nomellc <127232686+nomellc@users.noreply.github.com>
2023-10-14 14:22:05 +0900

    Create 연습문제 Ch9_3.java

---
+++ 4.java.lang 패키지와 유용한 클래스/정유진/연습문제 Ch9_3.java
@@ -0,0 +1,18 @@
+package nine;
+
+public class Exercise9_3 {
+
+	public static void main(String[] args) {
+		String fullPath = "c:\\jdk1.8\\work\\PathSeparateTest.java";
+		String path = "";
+		String fileName = "";
+		
+		path = fullPath.substring(0, 14);
+		fileName = fullPath.substring(15);
+		
+		System.out.println("fullPath:"+fullPath);
+		System.out.println("path:"+path);
+		System.out.println("fileName:"+fileName);
+	}
+
+}
